d72ec915a6c5c62945af860e78f3e08908efab82
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-04 16:22:48 +0930

    refactor reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
+export const SIGN_IN='SIGN_IN'
 export const signIn = () => {
     return {
         type: 'SIGN_IN'
     }
 }
 
+export const SIGN_OUT='SIGN_OUT'
 export const signOut = () => {
     return {
         type: 'SIGN_OUT'
