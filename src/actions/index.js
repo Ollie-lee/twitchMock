3cbe7bc59b275dc9a58b887925a2f9374638ee93
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-08 16:02:13 +1000

    create bulk action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams'
+import { formValues } from 'redux-form'
 
 export const SIGN_IN = 'SIGN_IN'
 export const signIn = (userId) => {
@@ -17,5 +18,50 @@ export const signOut = () => {
 
 export const CREATE_STREAM = 'CREATE_STREAM'
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues)
+    const response = await streams.post('/streams', formValues)//post a formValue to the server
+
+    dispatch({
+        type: 'CREATE_STREAM',
+        payload: response.data,
+    })
+}
+
+export const FETCH_STREAMS = 'FETCH_STREAMS';
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams')
+
+    dispatch({
+        type: 'FETCH_STREAMS',
+        payload: response.data
+    })
+}
+
+export const FETCH_STREAM = 'FETCH_STREAM';
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`)
+
+    dispatch({
+        type: 'FETCH_STREAM',
+        payload: response.data
+    })
+}
+
+export const EDIT_STREAM = 'EDIT_STREAM';
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues)
+
+    dispatch({
+        type: 'EDIT_STREAM',
+        payload: response.data
+    })
+}
+
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+
+    dispatch({
+        type: 'DELETE_STREAM',
+        payload: id
+    })
 }
