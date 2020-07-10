5c144eb787cd7a3e24473e7c74d2a68ea166bcc9
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-10 20:52:53 +1000

    fetch data independently in signle router

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { fetchStream } from '../../actions/index'
 
-const StreamEdit = () =>{
-    return <div>StreamEdit</div>
+import React from 'react'
+import { connect } from 'react-redux'
+
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        //first time this.props.stream is undefined
+        //after componentDidMount(), the stream data will show
+        
+        console.log(this.props)
+        return (
+            <div>
+                StreamEdit
+            </div>
+        )
+    }
+
+
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, {
+    fetchStream
+})(StreamEdit);
