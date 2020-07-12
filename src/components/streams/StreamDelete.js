cf76f18759a7fd63449d0772bdfa910ed3dac7c4
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-12 21:31:48 +1000

    fetch stream in delete stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,29 +1,51 @@
 import history from '../../history'
+import { fetchStream } from '../../actions'
 
+import { connect } from 'react-redux'
 import React from 'react'
 import Modal from '../Modal'
 
 
-const StreamDelete = () => {
-    const actions = (
-        <React.Fragment>
-            <button className='ui red button'>Delete</button>
-            <button className='ui button primary'>Cancel</button>
-        </React.Fragment>
-    )
+class StreamDelete extends React.Component {
+    // const actions = (
+    //     <React.Fragment>
+    //         <button className='ui red button'>Delete</button>
+    //         <button className='ui button primary'>Cancel</button>
+    //     </React.Fragment>
+    // )
 
-    return (
-        <div>
-            StreamDelete
-            <Modal
-                title='Delete Stream'
-                content='Are you sure you want to delete this stream?'
-                //we want to pass in two buttons in to actions, which means we need to pass jsx
-                actions={actions}
-                onDismiss={() => history.push('/')}
-            />
-        </div>
-    )
+    componentDidMount() {
+        //! id in the address bar:this.props.match.params.id
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    renderActions = () => {
+        return (
+            <React.Fragment>
+                <button className='ui red button'>Delete</button>
+                <button className='ui button primary'>Cancel</button>
+            </React.Fragment>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                StreamDelete
+                <Modal
+                    title='Delete Stream'
+                    content='Are you sure you want to delete this stream?'
+                    //we want to pass in two buttons in to actions, which means we need to pass jsx
+
+                    //we need () to call the result, we are not passing a reference
+                    actions={this.renderActions()}
+                    onDismiss={() => history.push('/')}
+                />
+            </div>
+        )
+    }
 }
 
-export default StreamDelete;
+export default connect(null, {
+    fetchStream
+})(StreamDelete);
