44da16b30243569e798977359f5be71191bec9ed
Ollie Lee <myself.ollie.lee@gmail.com>
2020-07-13 16:50:02 +1000

    connect delete api

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,14 @@
-import history from '../../history'
-import { fetchStream } from '../../actions'
-
 import { connect } from 'react-redux'
 import React from 'react'
 import Modal from '../Modal'
+import { Link } from 'react-router-dom'
+
+import history from '../../history'
+import {
+    fetchStream,
+    deleteStream,
+} from '../../actions'
+
 
 
 class StreamDelete extends React.Component {
@@ -20,21 +25,32 @@ class StreamDelete extends React.Component {
     }
 
     renderActions = () => {
+        const id = this.props.match.params.id
         return (
             <React.Fragment>
-                <button className='ui red button'>Delete</button>
-                <button className='ui button primary'>Cancel</button>
+                {/* not use this.props.deleteStream(id), because we can not pass id
+                which would be instantly called */}
+                <button onClick={() => this.props.deleteStream(id)} className='ui red button'>Delete</button>
+                <Link to='/' className='ui button primary'>Cancel</Link>
             </React.Fragment>
         )
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return 'Are you sure to delete this stream?'
+        }
+
+        return `Are you sure to delete the stream with title: ${this.props.stream.title}?`
     }
 
     render() {
         return (
             <div>
-                StreamDelete
+                {/* StreamDelete */}
                 <Modal
                     title='Delete Stream'
-                    content='Are you sure you want to delete this stream?'
+                    content={this.renderContent()}
                     //we want to pass in two buttons in to actions, which means we need to pass jsx
 
                     //we need () to call the result, we are not passing a reference
@@ -46,6 +62,15 @@ class StreamDelete extends React.Component {
     }
 }
 
-export default connect(null, {
-    fetchStream
+const mapStateToProps = (state, ownProps) => {
+    //use ownProps to access //! this.props.match.params.id
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+
+}
+
+export default connect(mapStateToProps, {
+    fetchStream,
+    deleteStream
 })(StreamDelete);
